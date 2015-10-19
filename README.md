e381eeea4757e861d99c39ec5712cd45f276f161
Licho <abuseware@users.noreply.github.com>
2015-10-19 04:10:35 +0200

    Removed BIOS interrupts

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Bare-metal Hello World
 ======================
 Simple x86 Assembly (NASM) example explaining how to make bootable "Hello World".
+Rewritten to work without BIOS interrupts. Enjoy, or use earlier commit.
 
 ![Screenshot](https://bitbucket.org/licho/baremetal-hello-world/raw/default/screenshot.png)
--- boot.s
+++ boot.s
@@ -1,8 +1,14 @@
 %define TERM_W 80
 %define TERM_H 25
+
+%define TERM_COLOR 0x02
+
 %define LOGO_X 48
 %define LOGO_Y 17
 
+%define GPU 0xB8000
+%define GPU_END GPU + (TERM_W) * (TERM_H) * 2
+%define LOGO_POS + (LOGO_X + (TERM_W * LOGO_Y)) * 2
 
 [BITS 16]
 org 0x7C00
@@ -22,59 +28,52 @@ data:
         times 224 - ( $ - $$ ) db 0x90
 
 main:
-    mov bx, 0x8
-    mov cx, 1
+
+cursor: ;Set cursor position to (0,0)
+    mov dx, 0x3D4 ;Prepare to send higher bits
+    mov al, 14
+    out dx, al
+    mov dx, 0x3D5 ;Send 0
+    mov al, 0
+    out dx, al
+    mov dx, 0x3D4 ;Prepare to send lower bits
+    mov al, 15
+    out dx, al
+    mov dx, 0x3D5 ;Send 0
+    mov al, 0
+    out dx, al
 
 clear:
-    mov al, " "
-    .putch:
-        mov ah, 2
-        int 0x10
-        mov ah, 9
-        inc dl
-        int 0x10
-        cmp dl, TERM_W
-        jnz clear.putch
-        xor dl, dl
-        inc dh
-        cmp dh, TERM_H
-        jnz clear.putch
+    mov edx, GPU ;Store GPU memory position
+    .write:
+    	mov [edx], BYTE " " ;Write space at current memory position
+    	mov [edx+1], BYTE TERM_COLOR ;Write color code
+        add edx, 2 ;Jump to next position
+        cmp edx, GPU_END ;Compare next position with end of GPU memory
+        jnz clear.write ;Repeat if not ended
 
 logo:
-    mov dl, LOGO_X
-    mov dh, LOGO_Y
-    mov ah, 2
-    int 0x10
-    mov si, data.str
+    mov edx, GPU + LOGO_POS ;Store starting position
+    mov ebx, 1 ;Store line counter
+    mov si, data.str ;Put string address for lodsb
     .print_char:
-        lodsb
-        cmp al, 0xA
-        jz logo.new_line
-        cmp al, 0x0
-        jz end
-        mov ah, 9
-        int 0x10
-        inc dl
-        mov ah, 2
-        int 0x10
-        jmp logo.print_char
+        lodsb ;Load char from [si]
+        cmp al, 0xA ;Check for new line
+        jz logo.new_line ;Make cariage return
+        cmp al, 0x0 ;Check for string end
+        jz end ;Finish
+        mov [edx], BYTE al ;Write char
+        add edx, 2 ;Jump to next position of memory
+        jmp logo.print_char ;Repeat
     .new_line:
-        inc dh
-        mov dl, LOGO_X
-        mov ah, 2
-        int 0x10
-        jmp logo.print_char
+    	mov edx, TERM_W * 2 ;Store terminal width
+    	imul edx, ebx ;Calculate position of line from counter
+    	add edx, GPU + LOGO_POS ;Add starting position
+    	inc ebx ;Increment counter
+        jmp logo.print_char ;Repeat
 
 end:
-    xor dx, dx
-    mov ah, 2
-    int 0x10
-    xor ax,ax
-    int 0x16
-    mov bx, 0x40
-    mov ds, bx
-    mov word [ds:0x72], 0x1234
-    jmp 0xffff:0
+    jmp end ;Loop forever
     
     .fill:
         times 510 - ( $ - $$ ) db 0x90
